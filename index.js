6c3354438348de0a8132c8e52499d7b6df95e349
henryau <henryau53@gmail.com>
2023-11-13 15:44:13 +0800

    feat: use Simplified Chinese commit type descriptions
    modify the import of conventional-commit-types module
    
    BREAKING CHANGE: change from conventional-commit-types to conventional-commit-types-zh-cn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'format cjs';
 
 var engine = require('./engine');
-var conventionalCommitTypes = require('conventional-commit-types');
+var conventionalCommitTypes = require('conventional-commit-types-zh-cn');
 var configLoader = require('commitizen').configLoader;
 
 var config = configLoader.load() || {};
